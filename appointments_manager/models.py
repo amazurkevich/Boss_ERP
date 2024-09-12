a1bdd1bb36fe0189276fe278cc4c86d7a196c8f6
Aleksey Mazurkevich <115803753+amazurkevich@users.noreply.github.com>
2024-09-13 00:25:45 +1000

    wip

--- appointments_manager/models.py
+++ appointments_manager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=32)
+    subject = models.CharField(max_length=32)
+    hourly_rate = models.IntegerField()
+
+    def __str__(self):
+        return str(self.name) + ' ' + str(self.subject)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    phone_number = models.CharField(max_length=12)
+
+    def __str__(self):
+        return str(self.name) + ' ' + str(self.surname)

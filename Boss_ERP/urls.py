3eeee099529af789ebf5df2dc808a327393070a8
amazurkevich <al.maz.7th@gmail.com>
2024-09-24 20:58:55 +1000

    wip

--- Boss_ERP/urls.py
+++ Boss_ERP/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("manage/", include("appointments_manager.urls")),
+    path("manage/", include("appointments.urls")),
     path("admin/", admin.site.urls),
 ]

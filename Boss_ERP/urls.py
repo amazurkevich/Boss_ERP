5f53a5b7afd1dc9c5ef67903acc02390a0e96639
amazurkevich <al.maz.7th@gmail.com>
2024-09-26 15:12:08 +1000

    wip

--- Boss_ERP/urls.py
+++ Boss_ERP/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("manage/", include("appointments.urls")),
     path("admin/", admin.site.urls),
+    path("", include("appointments.urls"))
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the appointments index.")
+    return render(request, 'Boss_ERP/index.html', context=None)
+
+def appointments_view(request):
+   return render(request, 'appointments/appointments.html', context=None)

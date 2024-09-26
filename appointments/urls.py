9ae2daad3039f192407d30db24258200ebb8903a
amazurkevich <al.maz.7th@gmail.com>
2024-09-26 22:06:09 +1000

    Teachers_page_view

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("appointments/", views.appointments_view, name="appointments")
+    path("appointments/", views.appointments_view, name="appointments"),
+    path("teachers/", views.all_teachers_view, name="teachers")
 ]
